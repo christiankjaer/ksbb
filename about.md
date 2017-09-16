71349cab3a071d3eab34b73c1f4eeab4a8069f61
Christian Kjær Larsen <christianden@gmail.com>
2017-09-16 16:00:35 +0200

    Stuff

---
+++ about.md
@@ -0,0 +1,18 @@
+---
+title: Om
+---
+
+King Size Big Band er et københavnsk Big Band med en nu 40-årig historie.
+
+Bandet har igennem årene opbygget et stort og unikt nodearkiv, der spænder bredt og kan varieres alt efter, om der er tale om en egentlig koncert - hvor komponister som Thad Jones og Bob Brookmeyer dominerer - eller om et dansearrangement, hvor det især er musik hentet fra Count Basies swingende univers, der er på repertoiret garneret med mere moderne funky toner.
+
+King Size Big Band har igennem årene arbejdet sammen med en række fremtrædende danske – og udenlandske musikere som Jesper Thilo, Etta Cameron, Andy Sheppard og Nils Landgren.
+
+King Size Big Band kan tilbyde at præsentere de bedste danske solister efter nærmere aftale.
+
+King Size Big Band holder til på det rytmiske konservatorium i København og blandt bandets besætning ses både garvede musikere og nye unge talenter.
+Dirigent for orkestret er den erfarne saxofonist og dirigent Thomas Stolting.
+
+King Size Big Band gør en ære ud af professionel optræden. Dette betyder: Musik til tiden, afstemt påklædning og fleksibilitet.
+
+Kontakt os og hør hvordan vi kan gøre Dit arrangement til noget helt specielt.
--- site.hs
+++ site.hs
@@ -1,54 +1,66 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
-import           Hakyll
 
+import Data.Monoid (mappend)
+import Hakyll
 
 --------------------------------------------------------------------------------
 main :: IO ()
-main = hakyll $ do
+main =
+  hakyll $ do
     match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
-
+      route idRoute
+      compile copyFileCompiler
     match "css/*.css" $ do
-        route   idRoute
-        compile compressCssCompiler
-
-    match (fromList ["about.rst", "contact.markdown"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
-
+      route idRoute
+      compile compressCssCompiler
+    match (fromList ["about.md"]) $ do
+      route $ setExtension "html"
+      compile $
+        pandocCompiler >>=
+        loadAndApplyTemplate "templates/about.html" defaultContext >>=
+        loadAndApplyTemplate "templates/default.html" defaultContext >>=
+        relativizeUrls
     match "musicians/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/musician.html" defaultContext
-
+      route $ setExtension "html"
+      compile $
+        pandocCompiler >>=
+        loadAndApplyTemplate "templates/musician.html" defaultContext
+    match "events/*" $ do
+      route $ setExtension "html"
+      compile $
+        pandocCompiler >>= loadAndApplyTemplate "templates/event.html" eventCtx
+    create ["events.html"] $ do
+      route idRoute
+      compile $ do
+        events <- chronological =<< loadAll "events/*"
+        let eventCtx =
+              constField "title" "Koncerter" `mappend`
+              listField "events" eventCtx (return events) `mappend`
+              defaultContext
+        makeItem "" >>= loadAndApplyTemplate "templates/events.html" eventCtx >>=
+          loadAndApplyTemplate "templates/default.html" eventCtx >>=
+          relativizeUrls
     create ["musicians.html"] $ do
-        route idRoute
-        compile $ do
-            musicians <- loadAll "musicians/*"
-            let musicianCtx =
-                    constField "title" "Musikere" `mappend`
-                    listField "musicians" defaultContext (return musicians) `mappend`
-                    defaultContext
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/musicians.html" musicianCtx
-                >>= loadAndApplyTemplate "templates/default.html" musicianCtx
-                >>= relativizeUrls
-
+      route idRoute
+      compile $ do
+        musicians <- loadAll "musicians/*"
+        let musicianCtx =
+              constField "title" "Musikere" `mappend`
+              listField "musicians" defaultContext (return musicians) `mappend`
+              defaultContext
+        makeItem "" >>=
+          loadAndApplyTemplate "templates/musicians.html" musicianCtx >>=
+          loadAndApplyTemplate "templates/default.html" musicianCtx >>=
+          relativizeUrls
     match (fromList ["index.html", "booking.html"]) $ do
-        route idRoute
-        compile $ getResourceBody >>= loadAndApplyTemplate "templates/default.html" defaultContext
-                        >>= relativizeUrls
-
+      route idRoute
+      compile $
+        getResourceBody >>=
+        loadAndApplyTemplate "templates/default.html" defaultContext >>=
+        relativizeUrls
     match "templates/*" $ compile templateBodyCompiler
 
-
 --------------------------------------------------------------------------------
-postCtx :: Context String
-postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+eventCtx :: Context String
+eventCtx = dateField "date" "%F" `mappend` defaultContext
